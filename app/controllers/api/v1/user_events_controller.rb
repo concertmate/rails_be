44c345fa1d4a1b79691b1d79062cbd895d3f9625
Clyde <clyde.autin@gmail.com>
2024-09-11 21:38:13 -0700

    user_events full crud complete

--- app/controllers/api/v1/user_events_controller.rb
+++ app/controllers/api/v1/user_events_controller.rb
@@ -3,4 +3,39 @@ class Api::V1::UserEventsController < ApplicationController
     user_event = UserEvent.find(params[:id])
     render json: UserEventSerializer.new(user_event)
   end
+
+  def create
+    user_event = UserEvent.new(user_event_params)
+    if user_event.save
+      render json: UserEventSerializer.new(user_event), status: :created
+    else
+      render json: { errors: user_event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    user_event = UserEvent.find(params[:id])
+
+    if user_event.destroy
+      render json: { message: "UserEvent has been deleted" }, status: :no_content
+    else
+      render json: { errors: user_event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    user_event = UserEvent.find(params[:id])
+    
+    if user_event.update(user_event_params)
+      render json: UserEventSerializer.new(user_event), status: :ok
+    else
+      render json: { errors: user_event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_event_params
+    params.require(:user_event).permit(:user_id, :event_id, :host)
+  end
 end
--- spec/requests/api/v1/user_events_request_spec.rb
+++ spec/requests/api/v1/user_events_request_spec.rb
@@ -28,4 +28,57 @@ RSpec.describe 'UserEvents API', type: :request do
     expect(user_event[:data][:attributes][:event_id]).to eq(@event1.id)
     expect(user_event[:data][:attributes][:host]).to eq(true)
   end
+
+  
+  it "creates a new user_event" do
+    user_id = @user2.id
+    event_id = @event1.id
+    host = false
+
+    post "/api/v1/user_events", params: { user_event: { user_id: user_id, event_id: event_id, host: host } }
+# require 'pry'; binding.pry
+    user_event = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(201)
+
+    expect(user_event).to be_a(Hash)
+    expect(user_event).to have_key(:data)
+
+    expect(user_event[:data][:attributes][:user_id]).to eq(user_id)
+    expect(user_event[:data][:attributes][:event_id]).to eq(event_id)
+    expect(user_event[:data][:attributes][:host]).to eq(host)
+  end  
+
+  it "updates an existing user_event" do
+    user_event_id = @user_event1.id
+    new_user_id = @user2.id
+    new_event_id = @event1.id
+    new_host = false
+
+    put "/api/v1/user_events/#{user_event_id}", params: { user_event: { user_id: new_user_id, event_id: new_event_id, host: new_host } }
+
+    user_event = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    expect(user_event).to be_a(Hash)
+    expect(user_event).to have_key(:data)
+
+    expect(user_event[:data][:attributes][:user_id]).to eq(new_user_id)
+    expect(user_event[:data][:attributes][:event_id]).to eq(new_event_id)
+    expect(user_event[:data][:attributes][:host]).to eq(new_host)
+  end
+
+  it "destroys an existing user_event" do
+    user_event_id = @user_event1.id
+
+    delete "/api/v1/user_events/#{user_event_id}"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(204)
+
+    expect(UserEvent.find_by(id: user_event_id)).to be_nil
+  end
 end

55f77b6ffcb4dbb6d944c0ec4a1b50d42c1474c9
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-09-18 19:42:24 -0600

    add the format of an email in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
   has_many :artists, through: :user_artists
 
   validates :name, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
 end
